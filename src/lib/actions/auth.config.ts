4312fa3f8e222b8f06c8094a63de768352f23aeb
celcius001 <itscelciusgaming@gmail.com>
2025-07-23 11:47:54 +0800

    login w/ google | github - ongoing

--- src/lib/actions/auth.config.ts
+++ src/lib/actions/auth.config.ts
@@ -1,8 +1,13 @@
 import Google from 'next-auth/providers/google';
 import GitHub from 'next-auth/providers/github';
 import type { NextAuthConfig } from 'next-auth';
+import { PrismaClient } from '@prisma/client';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+
+const prisma = new PrismaClient();
 
 export default {
+  adapter: PrismaAdapter(prisma),
   providers: [
     Google({
       authorization: {
